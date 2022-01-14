2347db7a640beaa4261a19fac739215318962ca7
Denis ALeksandrov <denis_aleksandrov_12@mail.ru>
2022-01-14 20:48:54 +0300

    add IP in env.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    authApi: 'http://localhost:5010/',
+    authApi: 'http://192.168.1.5:36746/',
     storeApi: 'http://localhost:5000/',
     whitelistedDomains: ['localhost:5000']
   }
